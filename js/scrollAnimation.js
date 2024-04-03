6ed15170d6c76149dcc6385423baa36c62a77436
RadojkoRadulovic <radoykoradulovic@gmail.com>
2024-04-03 12:41:25 -0400

    Change skils section

--- js/scrollAnimation.js
+++ js/scrollAnimation.js
@@ -11,18 +11,14 @@ const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 
 function checkBoxes() {
-    // console.log(window.innerHeight / 5 * 4);
     const triggerBottom = window.innerHeight / 5 * 4;
 
     boxes.forEach((box) => {
-        // console.log(box.getBoundingClientRect().top);
        const boxTop = box.getBoundingClientRect().top;
 
         if(boxTop < triggerBottom) {
-            // console.log("show");
             box.classList.add('show');
         } else {
-            // console.log("hide");
             box.classList.remove('show');
         }
 
@@ -34,26 +30,18 @@ const mySkillbox = document.querySelectorAll('.boxEl')
 window.addEventListener('scroll', checkMySkill);
 
 function checkMySkill() {
-    // console.log(window.innerHeight / 5 * 4);
     const triggerBottom = window.innerHeight / 5 * 4;
 
     mySkillbox.forEach((box) => {
-        // console.log(box.getBoundingClientRect().top);
        const boxTop = box.getBoundingClientRect().top;
 
         if(boxTop < triggerBottom) {
-            // console.log("show");
             box.classList.add('showBoxEl');
         } else {
-            // console.log("hide");
             box.classList.remove('showBoxEl');
         }
 
     });
     
 };
-
-
-
-
 export default scrollInit;
